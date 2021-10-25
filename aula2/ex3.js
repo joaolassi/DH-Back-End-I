ca8ec1155404b43e28e0a3006f4f0229d532d804
Joao Lassi <jmlassi@outlook.com>
2021-10-25 20:28:21 -0300

    Exercício 3

---
+++ aula2/ex3.js
@@ -0,0 +1,17 @@
+// crio as variáveis
+let a, b, resultado;
+// atribuo valores para elas
+a = 1;
+b = 1;
+// exibo o resultado da soma
+console.log("a + b =" + a + b);
+// atribuo a soma das variáveis a uma outra variável
+resultado = a + b;
+// exibo na tela
+console.log("Resultado: " + resultado);
+//somo o resultado com mais 1 e exibo
+resultado += 1;
+console.log("Resultado + 1 = " + resultado);
+// somo o resultado com mais 14 e exibo
+resultado += 14;
+console.log("Resultado + 14 = " + resultado);
