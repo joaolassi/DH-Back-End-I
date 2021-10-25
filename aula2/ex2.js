7d1f7a63bdcf29c3ec99feb5eaf7c2549af3f163
Joao Lassi <jmlassi@outlook.com>
2021-10-25 19:17:23 -0300

    Exercícios back-end

---
+++ aula2/ex2.js
@@ -0,0 +1,10 @@
+let bemVindo = "bem vindo";
+let quilometros = 25;
+let finalizado = true;
+
+console.log("Olá seja " + bemVindo + " ao programa!");
+console.log(bemVindo);
+console.log("Sua quilometragem: ");
+console.log(quilometros);
+console.log("Corrida finalizada?");
+console.log(finalizado);
